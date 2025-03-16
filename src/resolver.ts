a21f6047604d93ddb5cfb964809bf5d09a700f5d
Eduardo Ferraz <eduardozf1@gmail.com>
2025-03-16 15:51:34 -0300

    :memo: study: explore GraphQL related data and type relationships
    - Study implementation of GraphQL type relationships (one-to-many, many-to-one)
    - Learn how to connect entities through resolver-level relationships
    - Practice defining bidirectional relationships between games, reviews, and authors
    - Explore how to structure resolvers by type for better organization
    - Implement field-level resolvers to handle nested data fetching

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,24 +1,25 @@
 import * as db from "./_db";
 
-const gameResolvers = {
-  games: () => db.games,
-  game: (_parent, { id }) => db.games.find((game) => game.id === id),
-};
-
-const reviewResolvers = {
-  reviews: () => db.reviews,
-  review: (_parent, { id }) => db.reviews.find((review) => review.id === id),
-};
-
-const authorResolvers = {
-  authors: () => db.authors,
-  author: (_parent, { id }) => db.authors.find((author) => author.id === id),
-};
-
 export const resolvers = {
   Query: {
-    ...gameResolvers,
-    ...reviewResolvers,
-    ...authorResolvers,
+    games: () => db.games,
+    game: (_, { id }) => db.games.find((game) => game.id === id),
+    reviews: () => db.reviews,
+    review: (_, { id }) => db.reviews.find((review) => review.id === id),
+    authors: () => db.authors,
+    author: (_, { id }) => db.authors.find((author) => author.id === id),
+  },
+  Game: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.game_id === parent.id),
+  },
+  Review: {
+    game: (parent) => db.games.find((game) => game.id === parent.game_id),
+    author: (parent) =>
+      db.authors.find((author) => author.id === parent.author_id),
+  },
+  Author: {
+    reviews: (parent) =>
+      db.reviews.filter((review) => review.author_id === parent.id),
   },
 };
